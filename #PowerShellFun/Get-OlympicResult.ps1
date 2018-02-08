39167c88ff195d05ba17eaa112d9bc327d4b47ad
Forsbakk <jonas.forsbakk@horten.kommune.no>
2018-02-09 00:10:57 +0100

    changed prov to ES

--- #PowerShellFun/Get-OlympicResult.ps1
+++ #PowerShellFun/Get-OlympicResult.ps1
@@ -17,9 +17,114 @@ $Days = @(
     "https://www.olympic.org/pyeongchang-2018/results/en/cross-country-skiing/results-ladies-30km-mass-start-classic-fnl-000100-.htm"
 )
 
-$testURI = "https://www.olympic.org/pyeongchang-2018/results/en/luge/lugr073a-men-s-singles-trno-b00200-.htm"
+$URL = "https://www.eurosport.no/ol/alpint/event/alpint-utfor-menn/phase/asm010o01/"
+$WH = "https://outlook.office.com/webhook/a709d725-19f0-44cc-9a19-423144c0e90e@5eca1ec4-5a64-407b-9778-9f9147b7a293/IncomingWebhook/add628ae1f6e42879095aa7c724d148b/3eb3235c-90a4-4b04-8ec4-88c3dea3e446"
 
-$data = Invoke-WebRequest -Uri $testURI
+function Get-OLResults {
+        Param (
+            $URL
+        )
+    $page = Invoke-WebRequest $URL
+    $table = $page.ParsedHtml.body.getElementsByTagName('Table') | Select-Object Rows
+    $hRow = $true
 
-$table = $data.ParsedHtml.Body.getElementsByTagName("TABLE") | Select InnerHTML
-$table.InnerHTML
+    ForEach ($row in $table.rows) {
+        if ($hRow -eq $true) {
+            $hRow = $false
+        }
+        else {
+            $cells = $row.cells
+            $placement = $cells[0].innerText
+            $athlete = $cells[2].innerText
+            $athlete = $athlete.Trim()
+            $athleteFlag = $cells[2].getElementsByTagName("IMG") | Select-Object -ExpandProperty Src
+        }
+        $athleteFlag = $athleteFlag -replace "about:","https://www.eurosport.no"
+
+        $properties = @{
+            Place = $placement
+            Athlete = $athlete
+            athleteFlag = $athleteFlag
+        }
+        $obj = New-Object psobject -Property $properties
+        Write-Output $obj
+    }
+}
+
+Function Invoke-OLResults {
+    $data = Get-OLResults -URL $URL
+    $sec = @()
+    $BNF = $false
+    $first = $data | Where-Object { ($_.Place -eq "1") -or ($_.Place -eq "=1") }
+    foreach ($i in $first) {
+        $iProps = @{
+            "startGroup" = "true"
+            "activityTitle" = "**$($i.Athlete)**"
+            "activityText" = "Plassering: $($i.Place)"
+            "activityImage" = $i.athleteFlag
+        }
+        if ($i.athleteFlag -eq "https://www.eurosport.no/d3images/ml/flags/s/NOR.png") { 
+            $BNF = $true
+            $iProps += @{
+                "activitySubtitle" = "Beste norske"
+            }
+        }
+        $sec += $iProps
+    }
+    $second = $data | Where-Object { ($_.Place -eq "2") -or ($_.Place -eq "=2") }
+    foreach ($i in $second) {
+        $iProps = @{
+            "startGroup" = "true"
+            "activityTitle" = "**$($i.Athlete)**"
+            "activityText" = "Plassering: $($i.Place)"
+            "activityImage" = $i.athleteFlag
+        }
+        if ($i.athleteFlag -eq "https://www.eurosport.no/d3images/ml/flags/s/NOR.png") { 
+            $BNF = $true
+            $iProps += @{
+                "activitySubtitle" = "Beste norske"
+            }
+        }
+        $sec += $iProps
+    }
+    $third = $data | Where-Object { ($_.Place -eq "3") -or ($_.Place -eq "=3") }
+    foreach ($i in $third) {
+        $iProps = @{
+            "startGroup" = "true"
+            "activityTitle" = "**$($i.Athlete)**"
+            "activityText" = "Plassering: $($i.Place)"
+            "activityImage" = $i.athleteFlag
+        }
+        if ($i.athleteFlag -eq "https://www.eurosport.no/d3images/ml/flags/s/NOR.png") { 
+            $BNF = $true
+            $iProps += @{
+                "activitySubtitle" = "Beste norske"
+            }
+        }
+        $sec += $iProps
+    }
+    if ($BNF -eq $false) {
+        $bn = ($data | Where-Object { $_.athleteFlag -eq "https://www.eurosport.no/d3images/ml/flags/s/NOR.png" } | Measure-Object -Property place -Minimum).Minimum
+        $i = $data | Where-Object { $_.Place -eq $bn }
+        $iProps = @{
+            "startGroup" = "true"
+            "activityTitle" = "**$($i.Athlete)**"
+            "activityText" = "Plassering: $($i.Place)"
+            "activityImage" = $i.athleteFlag
+            "activitySubtitle" = "Beste norske"
+        }
+        $sec += $iProps
+    }
+    $properties = @{
+        "@type" = "MessageCard"
+        "@context" = "http://schema.org/extensions"
+        "summary" = "OL rapport"
+        "themeColor" = "0075FF"
+        "title" = "OL rapport"
+        "sections" = $sec
+    }
+    $obj = New-Object psobject -Property $properties
+    $body = $obj | ConvertTo-Json -Compress
+    Invoke-RestMethod -Uri $WH -Method Post -Body $body
+}
+Invoke-OLResults
