d581acde641c1b7e6aaf75a6def2b4a48ee671dc
Forsbakk <jonas.forsbakk@horten.kommune.no>
2018-07-14 09:43:53 +0200

    cleanup

--- Contunuous delivery for Intune v2/production/PowerShell/Create-Config.ps1
+++ Contunuous delivery for Intune v2/production/PowerShell/Create-Config.ps1
@@ -1,8 +1,8 @@
 $PowerShell = @(
     @{
         Name      = "Add Restart-Computer every night"
-        Command   = "Unregister-ScheduledTask -TaskPath '\' -TaskName 'Nightly Reboot' -Confirm:`$false; Register-ScheduledTask -Action (New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument '-Command Restart-Computer -Force') -Trigger (New-ScheduledTaskTrigger -Daily -At 09:00pm) -User 'SYSTEM' -RunLevel Highest -Settings (New-ScheduledTaskSettingsSet -DontStopIfGoingOnBatteries -DontStopOnIdleEnd -WakeToRun) -TaskName 'Nightly Rebootv0.3' -Description 'v0.3' -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries"
-        Detection = "[bool](Get-ScheduledTask -TaskName 'Nightly Rebootv0.3')"
+        Command   = "Register-ScheduledTask -Action (New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument '-Command Restart-Computer -Force') -Trigger (New-ScheduledTaskTrigger -Daily -At 09:00pm) -User 'SYSTEM' -RunLevel Highest -Settings (New-ScheduledTaskSettingsSet -DontStopIfGoingOnBatteries -DontStopOnIdleEnd -WakeToRun) -TaskName 'Nightly Reboot' -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries"
+        Detection = "[bool](Get-ScheduledTask -TaskName 'Nightly Reboot')"
     }
 )
 $PowerShell | ConvertTo-Json -Compress | Out-File "$PSScriptRoot\config.json" -Encoding default
--- Contunuous delivery for Intune v2/production/Regedit/Create-Config.ps1
+++ Contunuous delivery for Intune v2/production/Regedit/Create-Config.ps1
@@ -1,11 +1,11 @@
 $regfiles = @(
     @{
-        URL = ""
+        URL = "https://raw.githubusercontent.com/Forsbakk/Blog/master/Contunuous%20delivery%20for%20Intune%20v2/regfiles/OneDriveCfg.reg"
         detection = "[bool]((Get-ItemPropertyValue -Path `"HKLM:\Software\Policies\Microsoft\OneDrive`" -Name SilentAccountConfig) -eq 1)"
         Type = "HKLM"
     },
     @{
-        URL = ""
+        URL = "https://raw.githubusercontent.com/Forsbakk/Blog/master/Contunuous%20delivery%20for%20Intune%20v2/regfiles/ShownFileFmtPrompt.reg"
         detection = "[bool]((Get-ItemPropertyValue -Path REGISTRY::HKEY_USERS\.DEFAULT\Software\Microsoft\Office\16.0\Common\General -Name ShownFileFmtPrompt) -eq 1)"
         Type = "HKCU"
     }
