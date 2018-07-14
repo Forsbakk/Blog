89d068a68c702c83b18a11de646c30700f5ebfb5
Forsbakk <jonas.forsbakk@horten.kommune.no>
2018-07-14 14:59:02 +0200

    test

--- Contunuous delivery for Intune v2/versioncontrol/Create-Config.ps1
+++ Contunuous delivery for Intune v2/versioncontrol/Create-Config.ps1
@@ -1,11 +1,11 @@
 ﻿$Versions = @(
     @{
         Name    = "production"
-        Version = "1.0.1"
+        Version = "1.0.2"
     },
     @{
         Name    = "beta"
-        Version = "1.0.1"
+        Version = "1.0.2"
     }
 )
 $Versions | ConvertTo-Json -Compress | Out-File "$PSScriptRoot\config.json" -Encoding default
