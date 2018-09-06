aa10f304dbdfa627fc6be3932a872d1940eea449
Jonas Forsbakk <jonas@forsbakk.priv.no>
2018-09-06 13:44:13 +0200

    added code

--- Contunuous delivery for Intune v2/production/Choco/Create-Config.ps1
+++ Contunuous delivery for Intune v2/production/Choco/Create-Config.ps1
@@ -10,6 +10,14 @@ $ChocoPkgs = @(
     @{
         Name = "microsoft-teams.install"
         Mode = "install"
-    }
+    },
+    @{
+        Name = "vscode"
+        Mode = "install"
+    },
+    @{
+        Name = "vscode-powershell"
+        Mode = "install"
+    }  
 )
 $ChocoPkgs | ConvertTo-Json -Compress | Out-File "$PSScriptRoot\config.json" -Encoding default
