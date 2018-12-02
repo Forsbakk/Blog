fc10f7bbb001831c840e6ed1b7ea9f1337678e11
Jonas Forsbakk <jonfor@m365x720586.onmicrosoft.com>
2018-12-02 23:06:11 +0100

    comments

--- MDT AutoPilot/AutoPilot/Invoke-Sysprep.ps1
+++ MDT AutoPilot/AutoPilot/Invoke-Sysprep.ps1
@@ -1,6 +1,8 @@
 ﻿Param (
     $tsmanPID
 )
+#Wait for the task sequence to end
 Wait-Process $tsmanPID -ErrorAction SilentlyContinue
 
+#Run sysprep
 Start-Process "C:\Windows\System32\Sysprep\sysprep.exe" -ArgumentList "/oobe /quiet /reboot"
